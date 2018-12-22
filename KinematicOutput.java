578bc2677fc61ab5087a05fb2bc0a05b4010f21e
hiepdpt <38888712+hiepdpt@users.noreply.github.com>
2018-12-22 10:10:54 +0700

    Add files via upload

---
+++ KinematicOutput.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movement;
+
+/**
+ *
+ * @author User
+ */
+public class KinematicOutput {
+    
+    private Vector2D velocity;
+    private float rotation;
+
+    public KinematicOutput() {
+    }
+
+    public KinematicOutput(Vector2D velocity, float rotation) {
+        this.velocity = velocity;
+        this.rotation = rotation;
+    }
+
+    public Vector2D getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector2D velocity) {
+        this.velocity = velocity;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(float rotation) {
+        this.rotation = rotation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return "KinematicOutput{" + "velocity=" + velocity + ", rotation=" + rotation + '}';
+    }
+}
